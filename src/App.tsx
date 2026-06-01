5d31ddc9892dbbf3a881d9a9a7595d727ec27cea
madhu264m <madhu264m@gmail.com>
2026-06-02 02:27:17 +0530

    feat: mount full-screen tldraw wall with dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { Tldraw } from "tldraw";
+import "tldraw/tldraw.css";
 import "./App.css";
 
 export default function App() {
-  return <div className="app-root">Vibe Walls</div>;
+  return (
+    <div className="wall-root">
+      <Tldraw colorScheme="system" persistenceKey={undefined} />
+    </div>
+  );
 }
